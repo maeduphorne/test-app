6895dba3c081fe7dcd41d68997ee2fc0386210e6
Mae Duphorne <maelduphorne@gmail.com>
2021-10-07 13:23:26 -0600

    change recat app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO WORLD - I am an App using CI
         </p>
         <a
           className="App-link"
